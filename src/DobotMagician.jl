4dff9ddbbf67dbdc13558b6037d88d41c830372e
David AW Barton <david.barton@bristol.ac.uk>
2020-10-15 14:44:55 +0100

    Updated format for Commands

--- src/DobotMagician.jl
+++ src/DobotMagician.jl
@@ -208,7 +208,7 @@ function _write_var_as_type(T::Type{<:Tuple}, var, varsym)
         for (i, TT) in enumerate(T.parameters)
             push!(body.args, _write_var_as_type(TT, var <: Tuple ? var.parameters[i] : eltype(var), :($(varsym)[$i])))
         end
-        push!(body.args, :(return nothing))
+        push!(body.args, :(nothing))
         return body
     end
 end
@@ -222,7 +222,7 @@ function construct_command(cmd::Command{P}, payload, queue::Bool) where {P}
     # Uses dispatch to enforce correct payload types
     (!cmd.allow_queue && queue) && throw(ArgumentError("Command cannot be queued"))
     # Construct the message
-    io = IOBuffer(; sizehint=sz + 4)
+    io = IOBuffer()
     write(io, 0xaaaa)
     write(io, 0x0) # write len later
     write(io, cmd.id)
@@ -284,77 +284,77 @@ end
 
 #! format: off
 
-# # Commands - Device information
-# const set_device_sn = Command(0, true, false, :device_sn=>String, (), "Set device serial number")
-# const get_device_sn = Command(0, false, false, (), :device_sn=>String, "Get device serial number")
-# const set_device_name = Command(1, true, false, :device_name=>String, (), "Set device name")
-# const get_device_name = Command(1, false, false, (), :device_name=>String, "Get device name")
-# const get_device_version = Command(2, false, false, (), (:major_version=>UInt8, :minor_version=>UInt8, :revision=>UInt8), "Get device version")
-# const set_device_with_l = Command(3, true, false, (:is_with_l=>UInt8, :version=>UInt8), (), "Set sliding rail enable status")
-# const get_device_with_l = Command(3, false, false, (), :is_with_l=>UInt8, "Get sliding rail enable status")
-# const get_device_time = Command(4, false, false, (), :g_systick=>UInt32, "Get device time")
-# const get_device_id = Command(5, false, false, (), (:device_id1=>UInt32, :device_id2=>UInt32, :device_id3=>UInt32), "Get device ID")
+# Commands - Device information
+const set_device_sn = Command(0, true, false, (device_sn=String,), "Set device serial number")
+const get_device_sn = Command(0, false, false, (device_sn=String,), "Get device serial number")
+const set_device_name = Command(1, true, false, (device_name=String,), "Set device name")
+const get_device_name = Command(1, false, false, (device_name=String,), "Get device name")
+const get_device_version = Command(2, false, false, (major_version=UInt8, minor_version=UInt8, revision=UInt8), "Get device version")
+const set_device_with_l = Command(3, true, false, (is_with_l=UInt8, version=UInt8), "Set sliding rail enable status")
+const get_device_with_l = Command(3, false, false, (is_with_l=UInt8,), "Get sliding rail enable status")
+const get_device_time = Command(4, false, false, (g_systick=UInt32,), "Get device time")
+const get_device_id = Command(5, false, false, (device_id=NTuple{3,UInt32},), "Get device ID")
 
-# # Commands - Real-time pose
-# const get_pose = Command(10, false, false, (), (:x=>Float32, :y=>Float32, :z=>Float32, :r=>Float32, :J1=>Float32, :J2=>Float32, :J3=>Float32, :J4=>Float32), "Get the real-time pose")
-# const reset_pose = Command(11, true, false, (:manual=>UInt8, :rear_arm_angle=>Float32, :front_arm_angle=>Float32), (), "Reset the real-time pose")
-# const get_pose_l = Command(13, false, false, (), :pose_l=>Float32, "Get the real-time pose of the sliding rail")
+# Commands - Real-time pose
+const get_pose = Command(10, false, false, (xyzr=NTuple{4,Float32}, J1=NTuple{4,Float32}), "Get the real-time pose")
+const reset_pose = Command(11, true, false, (manual=UInt8, rear_arm_angle=Float32, front_arm_angle=Float32), "Reset the real-time pose")
+const get_pose_l = Command(13, false, false, (pose_l=Float32,), "Get the real-time pose of the sliding rail")
 
-# # Commands - Alarm
-# const get_alarms_state = Command(20, false, false, (), :alarms_state=>Vector{UInt8}, "Get the alarms state")
-# const clear_all_alarms_state = Command(21, true, false, (), (), "Clear the alarms state")
+# Commands - Alarm
+const get_alarms_state = Command(20, false, false, (alarms_state=Vector{UInt8},), "Get the alarms state")
+const clear_all_alarms_state = Command(21, true, false, (), "Clear the alarms state")
 
-# # Commands - Homing
-# const set_home_params = Command(30, true, true, (:x=>Float32, :y=>Float32, :z=>Float32, :r=>Float32), (), "Set the homing position")
-# const get_home_params = Command(30, false, false, (), (:x=>Float32, :y=>Float32, :z=>Float32, :r=>Float32), "Get the homing position")
-# const set_home_cmd = Command(31, true, true, :reserved=>UInt32, (), "Execute the homing function")
-# const set_auto_leveling = Command(32, true, true, (:is_autoleveling=>UInt8, :accuracy=>Float32), (), "Set automatic leveling")
-# const get_auto_leveling = Command(32, false, false, (), :result=>Float32, "Get automatic leveling result")
+# Commands - Homing
+const set_home_params = Command(30, true, true, (x=Float32, y=Float32, z=Float32, r=Float32), "Set the homing position")
+const get_home_params = Command(30, false, false, (x=Float32, y=Float32, z=Float32, r=Float32), "Get the homing position")
+const set_home_cmd = Command(31, true, true, (reserved=UInt32,), "Execute the homing function")
+const set_auto_leveling = Command(32, true, true, (is_autoleveling=UInt8, accuracy=Float32), "Set automatic leveling")
+const get_auto_leveling = Command(32, false, false, (result=Float32,), "Get automatic leveling result")
 
-# # Commands - Handhold teaching
-# const set_hht_trig_mode = Command(40, true, false, :hht_trig_mode=>UInt64, (), "Set hand-hold teaching mode")
-# const get_hht_trig_mode = Command(40, false, false, (), :hht_trig_mode=>UInt64, "Get hand-hold teaching mode")
-# const set_hht_trig_output_enabled = Command(41, true, false, :is_enabled=>UInt8, (), "Set status of hand-hold teaching mode")
-# const get_hht_trig_output_enabled = Command(41, false, false, (), :is_enabled=>UInt8, "Get status of hand-hold teaching mode")
-# const get_hht_trig_output = Command(42, false, false, (), :is_triggered=>UInt8, "Get the status of the hand-hold trigger")
+# Commands - Handhold teaching
+const set_hht_trig_mode = Command(40, true, false, (hht_trig_mode=UInt64,), "Set hand-hold teaching mode")
+const get_hht_trig_mode = Command(40, false, false, (hht_trig_mode=UInt64,), "Get hand-hold teaching mode")
+const set_hht_trig_output_enabled = Command(41, true, false, (is_enabled=UInt8,), "Set status of hand-hold teaching mode")
+const get_hht_trig_output_enabled = Command(41, false, false, (is_enabled=UInt8,), "Get status of hand-hold teaching mode")
+const get_hht_trig_output = Command(42, false, false, (is_triggered=UInt8,), "Get the status of the hand-hold trigger")
 
-# # Commands - End effector
-# const set_end_effector_params = Command(60, true, true, (:x_bias=>Float32, :y_bias=>Float32, :z_bias=>Float32), (), "Set the offset of the end effector")
-# const get_end_effector_params = Command(60, false, false, (), (:x_bias=>Float32, :y_bias=>Float32, :z_bias=>Float32), "Get the offset of the end effector")
-# const set_end_effector_laser = Command(61, true, true, (:enable_ctrl=>UInt8, :on=>UInt8), (), "Set the status of the laser")
-# const get_end_effector_laser = Command(61, false, false, (), (:is_ctrl_enabled=>UInt8, :is_on=>UInt8), "Get the status of the laser")
-# const set_end_effector_suction_cup = Command(62, true, true, (:is_ctrl_enabled=>UInt8, :is_sucked=>UInt8), (), "Set the status of the suction cup")
-# const get_end_effector_suction_cup = Command(62, false, false, (), (:is_ctrl_enabled=>UInt8, :is_sucked=>UInt8), "Get the status of the suction cup")
-# const set_end_effector_gripper = Command(63, true, true, (:is_ctrl_enabled=>UInt8, :is_gripped=>UInt8), (), "Set the status of the gripper")
-# const get_end_effector_gripper = Command(63, false, false, (), (:is_ctrl_enabled=>UInt8, :is_gripped=>UInt8), "Set the status of the gripper")
+# Commands - End effector
+const set_end_effector_params = Command(60, true, true, (x_bias=Float32, y_bias=Float32, z_bias=Float32), "Set the offset of the end effector")
+const get_end_effector_params = Command(60, false, false, (x_bias=Float32, y_bias=Float32, z_bias=Float32), "Get the offset of the end effector")
+const set_end_effector_laser = Command(61, true, true, (enable_ctrl=UInt8, on=UInt8), "Set the status of the laser")
+const get_end_effector_laser = Command(61, false, false, (is_ctrl_enabled=UInt8, is_on=UInt8), "Get the status of the laser")
+const set_end_effector_suction_cup = Command(62, true, true, (is_ctrl_enabled=UInt8, is_sucked=UInt8), "Set the status of the suction cup")
+const get_end_effector_suction_cup = Command(62, false, false, (is_ctrl_enabled=UInt8, is_sucked=UInt8), "Get the status of the suction cup")
+const set_end_effector_gripper = Command(63, true, true, (is_ctrl_enabled=UInt8, is_gripped=UInt8), "Set the status of the gripper")
+const get_end_effector_gripper = Command(63, false, false, (is_ctrl_enabled=UInt8, is_gripped=UInt8), "Set the status of the gripper")
 
-# # Commands - Jog
-# const set_jog_joint_params = Command(70, true, true, (:velocity_J1=>Float32,:velocity_J2=>Float32,:velocity_J3=>Float32,:velocity_J4=>Float32,:acceleration_J1=>Float32,:acceleration_J2=>Float32,:acceleration_J3=>Float32,:acceleration_J4=>Float32), (), "Set the velocity and acceleration of joints in jogging mode")
-# const get_jog_joint_params = Command(70, false, false, (), (:velocity_J1=>Float32,:velocity_J2=>Float32,:velocity_J3=>Float32,:velocity_J4=>Float32,:acceleration_J1=>Float32,:acceleration_J2=>Float32,:acceleration_J3=>Float32,:acceleration_J4=>Float32), "Get the velocity and acceleration of joints in jogging mode")
-# const set_jog_coordinate_params = Command(71, true, true, (:velocity_x=>Float32,:velocity_y=>Float32,:velocity_z=>Float32,:velocity_r=>Float32,:acceleration_x=>Float32,:acceleration_y=>Float32,:acceleration_z=>Float32,:acceleration_r=>Float32), (), "Set the velocity and acceleration in Cartesian coordinates in jogging mode")
-# const get_jog_coordinate_params = Command(71, false, false, (), (:velocity_x=>Float32,:velocity_y=>Float32,:velocity_z=>Float32,:velocity_r=>Float32,:acceleration_x=>Float32,:acceleration_y=>Float32,:acceleration_z=>Float32,:acceleration_r=>Float32), "Get the velocity and acceleration in Cartesian coordinates in jogging mode")
-# const set_jog_common_params = Command(72, true, true, (:velocity_ratio=>Float32, :acceleration_ratio=>Float32), (), "Set the velocity and acceleration ratios of the sliding rail")
-# const get_jog_common_params = Command(72, false, false, (), (:velocity_ratio=>Float32, :acceleration_ratio=>Float32), "Get the velocity and acceleration ratios of the sliding rail")
-# const set_jog_cmd = Command(73, true, true, (:is_joint=>UInt8, :cmd=>UInt8), (), "Execute the jog command in Cartesian coordinates or joints (cmd=0 stop, cmd=1 or 2 for positive or negative jogging in x/J1, cmd=3 or 4 for y/J2, etc)")
-# const set_jog_l_params = Command(74, true, true, (:velocity=>Float32, :acceleration=>Float32), (), "Set the velocity and acceleration of the sliding rail in jog mode") # NOTE: there is a discrepancy in the manual regarding the data types and overall length of the message
+# Commands - Jog
+const set_jog_joint_params = Command(70, true, true, (velocity=NTuple{4,Float32},acceleration=NTuple{4,Float32}), "Set the velocity and acceleration of joints in jogging mode")
+const get_jog_joint_params = Command(70, false, false, (velocity=NTuple{4,Float32},acceleration=NTuple{4,Float32}), "Get the velocity and acceleration of joints in jogging mode")
+const set_jog_coordinate_params = Command(71, true, true, (velocity=NTuple{4,Float32},acceleration=NTuple{4,Float32}), "Set the velocity and acceleration in Cartesian coordinates in jogging mode")
+const get_jog_coordinate_params = Command(71, false, false, (velocity=NTuple{4,Float32},acceleration=NTuple{4,Float32}), "Get the velocity and acceleration in Cartesian coordinates in jogging mode")
+const set_jog_common_params = Command(72, true, true, (velocity_ratio=Float32, acceleration_ratio=Float32), "Set the velocity and acceleration ratios of the sliding rail")
+const get_jog_common_params = Command(72, false, false, (velocity_ratio=Float32, acceleration_ratio=Float32), "Get the velocity and acceleration ratios of the sliding rail")
+const set_jog_cmd = Command(73, true, true, (is_joint=UInt8, cmd=UInt8), "Execute the jog command in Cartesian coordinates or joints (cmd=0 stop, cmd=1 or 2 for positive or negative jogging in x/J1, cmd=3 or 4 for y/J2, etc)")
+const set_jog_l_params = Command(74, true, true, (velocity=Float32, acceleration=Float32), "Set the velocity and acceleration of the sliding rail in jog mode") # NOTE: there is a discrepancy in the manual regarding the data types and overall length of the message
 
-# # Commands - PTP
-# const set_ptp_joint_params = Command(80, true, true, (:velocity_J1=>Float32,:velocity_J2=>Float32,:velocity_J3=>Float32,:velocity_J4=>Float32,:acceleration_J1=>Float32,:acceleration_J2=>Float32,:acceleration_J3=>Float32,:acceleration_J4=>Float32), (), "Set the velocity and acceleration of the joint coordinate axes in PTP mode")
-# const get_ptp_joint_params = Command(80, false, false, (), (:velocity_J1=>Float32,:velocity_J2=>Float32,:velocity_J3=>Float32,:velocity_J4=>Float32,:acceleration_J1=>Float32,:acceleration_J2=>Float32,:acceleration_J3=>Float32,:acceleration_J4=>Float32), "Get the velocity and acceleration of the joint coordinate axes in PTP mode")
-# const set_ptp_coordinate_params = Command(81, true, true, (:xyz_velocity=>Float32, :r_velocity=>Float32, :xyz_acceleration=>Float32, :r_acceleration=>Float32), (), "Set the velocity and acceleration of the Cartesian coordinate axes in PTP mode")
-# const get_ptp_coordinate_params = Command(81, false, false, (), (:xyz_velocity=>Float32, :r_velocity=>Float32, :xyz_acceleration=>Float32, :r_acceleration=>Float32), "Get the velocity and acceleration of the Cartesian coordinate axes in PTP mode")
-# const set_ptp_jump_params = Command(82, true, true, (:jump_height=>Float32, :z_limit=>Float32), (), "Set the lifting height and maximum lifting height in JUMP mode")
-# const get_ptp_jump_params = Command(82, false, false, (), (:jump_height=>Float32, :z_limit=>Float32), "Get the lifting height and maximum lifting height in JUMP mode")
-# const set_ptp_common_params = Command(83, true, true, (:velocity_ratio=>Float32, :acceleration_ratio=>Float32), (), "Set the velocity and acceleration ratio in PTP mode")
-# const get_ptp_common_params = Command(83, false, false, (:velocity_ratio=>Float32, :acceleration_ratio=>Float32), (), "Set the velocity and acceleration ratio in PTP mode")
-# const set_ptp_cmd = Command(84, true, true, (:ptp_mode=>UInt8, :x=>Float32, :y=>Float32, :z=>Float32, :r=>Float32), (), "Execute a PTP command (ptp_mode: 0=JUMP_XYZ, 1=MOVJ_XYZ, 2=MOVL_XYZ, 3=JUMP_ANGLE, 4=MOVJ_ANGLE, 5=MOVL_ANGLE, 6=MOVJ_INC, 7=MOVL_INC, 8=MOVJ_XYZ_INC, 9=JUMP_MOVL_XYZ)")
-# const set_ptp_l_params = Command(85, true, true, (:velocity=>Float32, :acceleration=>Float32), (), "Set the velocity and acceleration of the sliding rail in PTP mode")
-# const set_ptp_with_l_cmd = Command(86, true, true, (:ptp_mode=>UInt8, :x=>Float32, :y=>Float32, :z=>Float32, :r=>Float32, :l=>Float32), (), "Execute a PTP command with the sliding rail (ptp_mode: 0=JUMP_XYZ, 1=MOVJ_XYZ, 2=MOVL_XYZ, 3=JUMP_ANGLE, 4=MOVJ_ANGLE, 5=MOVL_ANGLE, 6=MOVJ_INC, 7=MOVL_INC, 8=MOVJ_XYZ_INC, 9=JUMP_MOVL_XYZ)")
-# const set_ptp_jump2_params = Command(87, true, true, (:start_jump_height=>Float32, :end_jump_height=>Float32, :z_limit=>Float32), (), "Set the extended parameters in JUMP mode")
-# const get_ptp_jump2_params = Command(87, false, false, (), (:start_jump_height=>Float32, :end_jump_height=>Float32, :z_limit=>Float32), "Get the extended parameters in JUMP mode")
+# Commands - PTP
+const set_ptp_joint_params = Command(80, true, true, (velocity=NTuple{4,Float32},acceleration=NTuple{4,Float32}), "Set the velocity and acceleration of the joint coordinate axes in PTP mode")
+const get_ptp_joint_params = Command(80, false, false, (velocity=NTuple{4,Float32},acceleration=NTuple{4,Float32}), "Get the velocity and acceleration of the joint coordinate axes in PTP mode")
+const set_ptp_coordinate_params = Command(81, true, true, (xyz_velocity=Float32, r_velocity=Float32, xyz_acceleration=Float32, r_acceleration=Float32), "Set the velocity and acceleration of the Cartesian coordinate axes in PTP mode")
+const get_ptp_coordinate_params = Command(81, false, false, (xyz_velocity=Float32, r_velocity=Float32, xyz_acceleration=Float32, r_acceleration=Float32), "Get the velocity and acceleration of the Cartesian coordinate axes in PTP mode")
+const set_ptp_jump_params = Command(82, true, true, (jump_height=Float32, z_limit=Float32), "Set the lifting height and maximum lifting height in JUMP mode")
+const get_ptp_jump_params = Command(82, false, false, (jump_height=Float32, z_limit=Float32), "Get the lifting height and maximum lifting height in JUMP mode")
+const set_ptp_common_params = Command(83, true, true, (velocity_ratio=Float32, acceleration_ratio=Float32), "Set the velocity and acceleration ratio in PTP mode")
+const get_ptp_common_params = Command(83, false, false, (velocity_ratio=Float32, acceleration_ratio=Float32), "Set the velocity and acceleration ratio in PTP mode")
+const set_ptp_cmd = Command(84, true, true, (ptp_mode=UInt8, x=Float32, y=Float32, z=Float32, r=Float32), "Execute a PTP command (ptp_mode: 0=JUMP_XYZ, 1=MOVJ_XYZ, 2=MOVL_XYZ, 3=JUMP_ANGLE, 4=MOVJ_ANGLE, 5=MOVL_ANGLE, 6=MOVJ_INC, 7=MOVL_INC, 8=MOVJ_XYZ_INC, 9=JUMP_MOVL_XYZ)")
+const set_ptp_l_params = Command(85, true, true, (velocity=Float32, acceleration=Float32), "Set the velocity and acceleration of the sliding rail in PTP mode")
+const set_ptp_with_l_cmd = Command(86, true, true, (ptp_mode=UInt8, x=Float32, y=Float32, z=Float32, r=Float32, l=Float32), "Execute a PTP command with the sliding rail (ptp_mode: 0=JUMP_XYZ, 1=MOVJ_XYZ, 2=MOVL_XYZ, 3=JUMP_ANGLE, 4=MOVJ_ANGLE, 5=MOVL_ANGLE, 6=MOVJ_INC, 7=MOVL_INC, 8=MOVJ_XYZ_INC, 9=JUMP_MOVL_XYZ)")
+const set_ptp_jump2_params = Command(87, true, true, (start_jump_height=Float32, end_jump_height=Float32, z_limit=Float32), "Set the extended parameters in JUMP mode")
+const get_ptp_jump2_params = Command(87, false, false, (start_jump_height=Float32, end_jump_height=Float32, z_limit=Float32), "Get the extended parameters in JUMP mode")
 
 #! format: on
 
-# include("Direct.jl")
+include("Direct.jl")
 
 end
